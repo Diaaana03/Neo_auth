59ff305e6a897e37b93e9da43b4b4f3d36128e5c
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-07-10 01:38:21 +0100

    register page added

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,6 +1,49 @@
 import React from "react";
 import classes from "./Register.module.css";
+import { Link } from "react-router-dom";
+import loginImg from "../../Assets/Images/LoginImg.svg";
+import backArrow from "../../Assets/Images/backArrow.svg";
 
 export const Register = () => {
-  return <div>vdc</div>;
+  return (
+    <div className={classes.register__section}>
+      <div className={classes.left}>
+        <div className={classes.login__img}>
+          <img src={loginImg} alt="lorby" />
+        </div>
+        <div>
+          <h1>Lorby</h1>
+          <p>Your personal tutor</p>
+        </div>
+      </div>
+      <div className={classes.right}>
+        <Link to="/">
+          <div className={classes.back__btn}>
+            <img className={classes.arrow__img} src={backArrow} alt="arrow" />
+            Back to the login page
+          </div>
+        </Link>
+        <h2 className={classes.right__h2}>Create a Lorby account</h2>
+        <form>
+          <div className={classes.input__container}>
+            <input type="text" id="login" placeholder="Email" name="login" />
+            <input type="text" id="login" placeholder="Login" name="login" />
+            <input
+              placeholder="Create a password"
+              name="password"
+              id="password"
+            />
+            <input
+              placeholder="Repeat a password"
+              name="password"
+              id="password"
+            />
+          </div>
+          <button type="submit" className={classes.register__btn}>
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
